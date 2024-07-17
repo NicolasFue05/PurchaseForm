633506d795d938e9c5fa3033939b1572234948b7
NicolasFue05 <nicolasfue05@gmail.com>
2024-07-17 02:43:21 -0500

    :feat: verify purchase function added / syntax fixed

--- app.js
+++ app.js
@@ -11,42 +11,42 @@ const passwordInput = document.getElementById('password');
 const showPasswordInput = document.getElementById('show-password');
 
 function checkoutAge(age){
-    submitForm.addEventListener('click', (e) => {
-        // evitar que se recargue la pagina
-        e.preventDefault();
-
-
-    })
+    if(age.value <= 18){
+            return false
+    } else{
+        return true
+    }
 }
 
 // check the type of credit card
 function checkCreditCard(){
-    submitForm.addEventListener('click', (e) => {
+    if(visaInput.checked){
+        console.log("Visa");
+        return true
+    } else if(mastercardInput.checked){
+        console.log("Mastercard")
+        return true
+    } else if(paypalInput.checked){
+        console.log("Paypal")
+        return true
+    } else {
         e.preventDefault();
-
-        if(visaInput.checked){
-            console.log("Visa");
-        } else if(mastercardInput.checked){
-            console.log("Mastercard")
-        } else if(paypalInput.checked){
-            console.log("Paypal")
-        } else {
-            message.innerText = "Your not subcribed!"
-            console.log("You are not subcribed!!")
-        }
-    })
+        message.innerText = "Your not subcribed!"
+        console.log("You are not subcribed!!")
+        return false
+    }
 }
 
 // Terms and Conditions
 function checkTermsAndConditions(){
-    submitForm.addEventListener('click', (e) => {
         if(termsInput.checked){
             console.log("Terms accepted!")
+            return true
         } else{
             console.log("Terms no accepted!")
             message.innerText = "You need to accept terms and conditions!"
+            return false
         }
-    })
 }
 
 // function to show the password
@@ -61,9 +61,19 @@ function showPassword() {
 });
 }
 
+// Show a message if the purchase if correct
+function verifyPurchase(){
+    submitForm.addEventListener('click', (e) => {
+            // checkout age
+        if(checkoutAge(ageInput) && checkCreditCard() && checkTermsAndConditions()){
+            message.innerText = "Purchase Complete!";
+        } else {
+            e.preventDefault()
+            message.innerText = "Error to Purchase!"
+        }
+    })
+}
 
 
+verifyPurchase()
 showPassword()
-checkCreditCard()
-checkTermsAndConditions()
-checkoutAge(ageInput)
