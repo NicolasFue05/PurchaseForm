9c4fea6beb895343e2c43832f540fe54976ddaa5
NicolasFue05 <nicolasfue05@gmail.com>
2024-07-17 01:42:35 -0500

    :feat: check credit card function added

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 // checkout form
 // constans
 const ageInput = document.getElementById('age');
-const ageMessage = document.getElementById('age-message');
+const message = document.getElementById('message');
 const submitForm = document.getElementById('enter-button');
-
+const visaInput = document.getElementById('visa');
+const mastercardInput = document.getElementById('mastercard');
+const paypalInput = document.getElementById('paypal');
 
 function checkoutAge(age){
     submitForm.addEventListener('click', (e) => {
         // evitar que se recargue la pagina
         e.preventDefault();
 
-
         if(age.value <= 18){
-            ageMessage.innerText = "You are a minor, you cannot buy!!";
+            message.innerText = "You are a minor, you cannot buy!!";
         } else {
-            ageMessage.innerHTML = "You are older!";
+            message.innerText = "You are older!";
         }
     })
 }
 
+// check the type of credit card
+function checkCreditCard(){
+    submitForm.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if(visaInput.checked){
+            console.log("Visa");
+        } else if(mastercardInput.checked){
+            console.log("Mastercard")
+        } else if(paypalInput.checked){
+            console.log("Paypal")
+        } else {
+            message.innerText = "Your not subcribed!"
+            console.log("You are not subcribed!!")
+        }
+    })
+}
+
+checkCreditCard()
 checkoutAge(ageInput)
